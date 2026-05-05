f2145c99fe1c3562bf621612215479260070a268
gkorzeno <130322551+gkorzeno@users.noreply.github.com>
2026-05-05 15:32:03 -0500

    Delete src directory

--- src/payoffs/Payoff.hpp
+++
@@ -1,8 +0,0 @@
-// src/payoffs/Payoff.hpp
-#pragma once
-
-class Payoff {
-public:
-    virtual double operator()(double S) const = 0;
-    virtual ~Payoff() {}
-};
--- src/stochasticProcess/CholeskyProcess.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "MultiDimensionalProcess.hpp"
-
-class CholeskyProcess : public MultiDimensionalProcess {
-public:
-    virtual bool diffusionIsCholesky() const final { return true; }
-};
